548f6c5a3daa5f049523a80498c27a617efef686
Robert Ris <take1risk@gmail.com>
2020-04-05 21:51:48 -0500

    implement modal

--- App.js
+++ App.js
@@ -2,9 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet,
          Text,
          View,
-         TextInput,
          Button,
-         ScrollView,
          FlatList } from 'react-native';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
@@ -12,12 +10,14 @@ import GoalInput from './components/GoalInput';
 export default function App() {
 
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
   const addGoalHandler = goalTitle => {
     setCourseGoals(currentGoals => [
       ...courseGoals,
       { id: Math.random().toString(), value: goalTitle }
       ]);
+      setIsAddMode(false);
   };
 
   const removeGoalHandler = goalId => {
@@ -29,17 +29,25 @@ export default function App() {
     });
   }
 
+  const cancelGoalHandler = () => {
+    setIsAddMode(false);
+  };
+
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button title="Add New Goal"
+        onPress={() => setIsAddMode(true)}/>
+      <GoalInput
+        onAddGoal={addGoalHandler}
+        visible={isAddMode}
+        onCancel={cancelGoalHandler}/>
       <FlatList
         data={courseGoals}
         renderItem={itemData =>
           <GoalItem
             id={itemData.item.id}
             onDelete={removeGoalHandler}
-            title={itemData.item.value} />}
-        />
+            title={itemData.item.value} />}/>
     </View>
   );
 }
